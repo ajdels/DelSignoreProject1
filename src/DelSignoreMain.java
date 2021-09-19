3c00b6bab26df1984d2b0c3383c58f1eb458d0e3
anthonydelsignore <adelsignore@student.bridgew.efu>
2021-09-19 19:13:15 -0400

    project 1

--- src/DelSignoreMain.java
+++ src/DelSignoreMain.java
@@ -1,2 +1,20 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 public class DelSignoreMain {
+    var filename = "List.txt";
+    var products = Paths.get(filename);
+    var productName = new ArrayList<String>();
+    var productPrice = new ArrayList<Float>();
+    var productAvailable = new ArrayList<Integer>();
+    var productInfo = Files.readAllLines(products);
+    for (var line : productInfo){
+        var splitLine = line.split(",");
+        productName.add(splitLine[0]);
+        var available = Integer.parseInt(splitLine[1]);
+        productAvailable.add(available);
+    }
+    for (int i = 0; 1< studentNames.size(); i++)
+        System.out.println("")
 }
