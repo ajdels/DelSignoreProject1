51549cd30da5564b37400da3072b1b750ecc2796
anthonydelsignore <adelsignore@student.bridgew.efu>
2021-09-21 20:54:52 -0400

    p1

--- src/DelSignoreMain.java
+++ src/DelSignoreMain.java
@@ -1,8 +1,11 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class DelSignoreMain {
+    public static void main(String[] args) {
     var filename = "List.txt";
     var products = Paths.get(filename);
     var productName = new ArrayList<String>();
@@ -14,7 +17,26 @@ public class DelSignoreMain {
         productName.add(splitLine[0]);
         var available = Integer.parseInt(splitLine[1]);
         productAvailable.add(available);
+        var userResponse = "";
+        while(userResponse.equals("done")) {
+            System.out.println("What product?: ");
+            userResponse = reader.nextLine();
+            if (userResponse.equals("done")) {
+                break;
+            }
+            productName.add(userResponse);
+            System.out.println("This is out of stock");
+            for (var productName : productNames);
+                System.out.print("Done?");
+
+            for (int i = 0; 1< productName.size(); i++)
+                System.out.println(productInfo);
+        }
+
+        ;
+        }
     }
-    for (int i = 0; 1< studentNames.size(); i++)
-        System.out.println("")
+
 }
+
+
